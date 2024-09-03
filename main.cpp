384883c4b0c0d2f7789c9ff7ed1eecb705d9d670
Dustin <42839971+DustinVII@users.noreply.github.com>
2024-09-03 08:02:02 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 // ╚══════════════╝
 const int ledPinRed = 14;
 const int ledPinGreen = 15;
-const int ledPinBlue = 16; //14. 2 for board LED on the ESP32 WROOM32
+const int ledPinBlue = 16; // Choose pin 2 for the board's build in blue LED on the ESP32 WROOM32
 
 // ╔════════════════════════════╗
 // ║ Server & Internet Settings ║
@@ -17,9 +17,9 @@ const char* password = "yourpasswordhere";
 
 void setup() {
     Serial.begin(115200);
-  pinMode(ledPinRed, OUTPUT); // Set the LED pin mode
-  pinMode(ledPinGreen, OUTPUT); // Set the LED pin mode
-  pinMode(ledPinBlue, OUTPUT); // Set the LED pin mode
+      pinMode(ledPinRed, OUTPUT); // Sets the LED pin mode
+      pinMode(ledPinGreen, OUTPUT); // Sets the LED pin mode
+      pinMode(ledPinBlue, OUTPUT); // Sets the LED pin mode
 
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
@@ -31,7 +31,7 @@ void setup() {
 
 
 void loop() {
-      //Lights
+      //Light show once WiFi is connected
   const int onTime = 10;
   const int offTime = 1500;
         digitalWrite(ledPinGreen, HIGH);
